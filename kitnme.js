e218e70958acd4cef42346dcafd31efd7553059c
captconrado <ottey001@gmail.com>
2013-03-23 11:42:48 -0700

    changed the players

--- kitnme.js
+++ kitnme.js
@@ -34,12 +34,12 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
+      var names = ["Player 1",
+                   "Kitty 1",
+                   "Player 2",
+                   "Kittey 2",
+                   "Player 3",
+                   "Player 3"];
       for (var i = 0; i < names.length; i++)
         Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
     }
